80f805280f22318e8fc75f25d0fa42da354dec56
mddazure <mddazure@gmail.com>
2020-10-17 16:02:54 +0200

    remove trf prov version

--- s2svpn.tf
+++ s2svpn.tf
@@ -1,14 +1,11 @@
 provider "azurerm" {
-version = "=2.0"
 features {}
 }
 #Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "s2srg"
+  name     = "s2srg-2"
   location = "West Central US"
 }
-
-
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "q1" {
